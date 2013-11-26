d7ab17f3e32820aa79cdd5e459b863a6c9d57310
Cedric Winbush <cawinbushjr@gmail.com>
2013-11-26 09:09:29 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+MhdemoApp::Application.config.secret_key_base = 'f85e179d087e44ff1dfa95554e6bbf01f1229cc80ca45ac50467ab68106857b67ed3e70e43e73262716c8d58af868c8f482e625977b8bc54ac91ce66d7a252dc'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+MhdemoApp::Application.config.session_store :cookie_store, key: '_mhdemo_app_session'
